c29db003ea538c1db1d5f0b7ec15dba3be820608
Learn Code With Durgesh Officials <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2023-11-02 13:22:29 +0530

    deployment issue fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/personal_website/",
+  base: "/personal_website_yt/",
   build: {
     outDir: "./docs",
   },
